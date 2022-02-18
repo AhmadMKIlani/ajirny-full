7479bb26910c26cde4a8e3b65e371fee4ac270b9
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2022-02-18 17:10:27 +0200

    new style

--- ajirny/src/App.js
+++ ajirny/src/App.js
@@ -1,7 +1,9 @@
 import Landing from './components/home/Landing';
+import {Nav} from "./components/nav/Nav";
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Landing/>
     </div>
   );
