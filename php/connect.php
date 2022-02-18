e734fac2504bf7b9f65b76f8a5ef0b640cac0a9c
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2022-02-18 13:38:58 +0200

    full project push

---
+++ php/connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "ajirny";
+
+// create connection 
+$db = mysqli_connect($servername, 
+$username ,
+$password ,
+$database );
+
+//check connection
+
+if($db->connect_error) {
+die("connection failed:" .$db->connect_error);
+}
+
+
+?>
