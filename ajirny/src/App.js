16c81edb77d3b7792d2dd3f03412935b50ed1375
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2022-02-19 09:54:36 +0200

    home page edit

--- ajirny/src/App.js
+++ ajirny/src/App.js
@@ -1,10 +1,18 @@
 import Landing from './components/home/Landing';
 import {Nav} from "./components/nav/Nav";
+import Category from './components/home/Lcategory/Category';
+import Footer from "./components/footer/Footer";
+import Testimonials from './components/testimonials/Testimonials';
+import { Partners } from './components/partners/Partners';
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Landing/>
+      <Category/>
+      <Testimonials/>
+      <Partners/>
+      <Footer/>
     </div>
   );
 }
--- ajirny/src/components/home/Landing.js
+++ ajirny/src/components/home/Landing.js
@@ -3,32 +3,17 @@ import "./Landing.css";
 
 
 const Landing = () => {
+
+ 
   return (
     <div>
-      <div class="main-background">
+    <div class="main-background">
         <div class="main-text">
             <h2>perfection</h2>
         <h1>and you are <span>invited</span></h1>
         </div>
-        
     </div> 
-    {/* <!-- background image ended --> */}
-
-{/* <!-- booking secton --> */}
-<div class="book-section">
-    <div class="inner-book">
-        <input type="text" placeholder="arrival"/>
-        <input type="text" placeholder="departure"/>
-        <input type="text" placeholder="choose room"/>
-        <input type="text" placeholder="state"/>
-        <div class="book-btn">
-            <a href="#">book now</a>
-        </div>
-    </div>
-    
-</div>
-{/* <!-- booking secton ended --> */}
-    </div>
+    </div> 
   
   )
 }
