48a15bffd7c341a52851d129c4b23ca847e8e4f7
NermeenAlahmad <nermeen.n.alahmad@gmail.com>
2022-02-19 13:47:55 +0200

    add

--- ajirny/src/App.js
+++ ajirny/src/App.js
@@ -1,10 +1,14 @@
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
 import Landing from './components/home/Landing';
 import {Nav} from "./components/nav/Nav";
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <Landing/>
+      {/* <Landing/> */}
+      <About/>
+      <Contact/>
     </div>
   );
 }
---
+++ ajirny/src/components/contact/Contact.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './contact.css'
+import { useForm } from "react-hook-form";
+// import axios from 'axios';
+
+function Contact() {
+
+    const { register, handleSubmit} = useForm();
+     const onSubmit = data => {
+         console.log(data)
+//         axios.post('http://localhost/ajirny-full/php/contact.php',data)
+//     .then(res=> console.log(res.data))
+//     .catch(error => {
+//       console.log(error.response)
+//   });
+     }
+      
+
+  return (
+    <div className="container">
+    <div className="content">
+      <div className="left-side">
+        <div className="address details">
+          <i className="fas fa-map-marker-alt"></i>
+          <div className="topic">Address</div>
+          <div className="text-one">Surkhet, NP12</div>
+          <div className="text-two">Birendranagar 06</div>
+        </div>
+        <div className="phone details">
+          <i className="fas fa-phone-alt"></i>
+          <div className="topic">Phone</div>
+          <div className="text-one">+0098 9893 5647</div>
+          <div className="text-two">+0096 3434 5678</div>
+        </div>
+        <div className="email details">
+          <i className="fas fa-envelope"></i>
+          <div className="topic">Email</div>
+          <div className="text-one">codinglab@gmail.com</div>
+          <div className="text-two">info.codinglab@gmail.com</div>
+        </div>
+      </div>
+      <div className="right-side"  >
+        <div className="topic-text">Send us a message</div>
+        <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
+      <form action="#"  onClick={handleSubmit(onSubmit)} >
+        <div className="input-box">
+          <input type="text" placeholder="Enter your name" name="name" ref={register} />
+        </div>
+        <div className="input-box">
+          <input type="text" placeholder="Enter your email" name="email" ref={register} />
+        </div>
+        <div className="input-box message-box">
+          <textarea name="" id="" cols="30" rows="10" placeholder="Enter your message" name="message" ></textarea>
+        </div>
+        <div className="button">
+          <input type="button" value="Send Now" />
+        </div>
+      </form>
+    </div>
+    </div>
+  </div>
+  )
+}
+
+export default Contact
