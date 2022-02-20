39c5037c6c9be40f1ba20d988cb01c0411c7d448
Sahar Zahran <61621196+Saharzahran@users.noreply.github.com>
2022-02-20 14:05:14 +0200

    nothing

--- ajirny/src/App.js
+++ ajirny/src/App.js
@@ -1,7 +1,7 @@
 
 import SinglePage from './components/singlepage/SinglePage';
 import Landing from './components/home/Landing';
-import {Nav} from "./components/nav/Nav";
+import { Nav } from "./components/nav/Nav";
 import Footer from "./components/footer/Footer";
 import Registration from './components/registration/Registration';
 import Login from './components/login/Login';
@@ -17,17 +17,18 @@ import Category from './components/category/Category';
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Nav/>
-      <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/categories" element={<Category/>}/>
-        <Route path="/registration" element={<Registration/>}/>
-        <Route path="/login" element={<Login/>}/>
-   
-      </Routes>
-      <Footer/>
-    </div>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/categories" element={<Category />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singlepage" element={<SinglePage />} />
+
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
